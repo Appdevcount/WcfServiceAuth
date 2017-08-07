670baa6acbbfaaf220549f14744d8386ddf3fd72
Appdevcount <Appdevcount@users.noreply.github.com>
2017-08-07 22:31:08 +0300

    Add project files.

---
+++ WcfService/App_Code/Authenticator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web;
+
+namespace WcfService.App_Code
+{
+    public class UsernameValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            // validate arguments
+            if (string.IsNullOrEmpty(userName))
+                throw new ArgumentNullException("userName");
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password");
+
+            // check if the user is not test
+            if (userName != "test" || password != "test")
+                throw new SecurityTokenException("Unknown username or password");
+        }
+    }
+}
